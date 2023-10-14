e553389546a676503e65fd015b206e92e720bf66
sepxx <mihartthx@gmail.com>
2023-10-14 18:20:57 +0300

    initial

---
+++ src/test/java/com/bnk/vtbmapsgamificationservice/VtbMapsGamificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnk.vtbmapsgamificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtbMapsGamificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
